eb9edea49a4bbc1bebe2450bc918b8069ded8bd6
Shadowmistlee <shadowmistlee@gmail.com>
2021-03-28 11:42:42 +0800

    20210328

---
+++ homework/20210328/caculator.py
@@ -0,0 +1,13 @@
+a = float(input('?'))
+b = str(input('?'))
+c = float(input('?'))
+if b =='*':
+    print(a*c)
+elif b =='+':
+    print(a+c)
+elif b =='/':
+    print(a/c)
+elif b =='-':
+    print(a-c)
+else:
+    print('WTF')
